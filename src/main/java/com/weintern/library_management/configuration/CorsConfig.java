6c69813e70af9aa4762b94c4923474bc4f6f7069
Thaitheya <thaitheyasudanpk@gmail.com>
2026-07-15 22:40:47 +0530

    Updated Server

--- src/main/java/com/weintern/library_management/configuration/CorsConfig.java
+++ src/main/java/com/weintern/library_management/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://feedback-management-system-sysslan.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
